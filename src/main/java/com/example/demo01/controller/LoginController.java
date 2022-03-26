953fb4b90520ac1fba76842970e2153f0015ed47
pll <3563390533@qq.com>
2022-03-26 21:37:25 +0800

    4

---
+++ src/main/java/com/example/demo01/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.demo01.controller;
+
+import com.example.demo01.service.TbUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+//public class LoginController {
+//
+//    @Autowired
+//    TbUserService tbUserService;
+//
+//    @RequestMapping("/login")
+//    public String show(){
+//        return "login";
+//    }
+//
+//    @RequestMapping
+//}
